38084e7b814c928c655aab500d67c0c0f406f9d8
philipbert0441 <philipb0441@gmail.com>
2023-08-26 17:59:15 -0400

    Initial functioning of App with static image

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,3 +1,6 @@
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -6,16 +9,38 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import io.kamel.image.KamelImage
+import io.kamel.image.asyncPainterResource
 
 @Composable
 internal fun App() {
     MaterialTheme {
-        var text by remember { mutableStateOf("Hello, World!") }
-
-        Button(onClick = {
-            text = "Hello, ${getPlatformName()}"
-        }) {
-            Text(text)
+        var text by remember { mutableStateOf("Hello, Philip!") }
+        var showImage by remember { mutableStateOf(false) }
+        Column(
+            Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Button(onClick = {
+                showImage = !showImage
+                text = if (showImage) {
+                    "Hello, ${getPlatformName()}"
+                } else {
+                    "Hello, Philip!"
+                }
+            }) {
+                Text(text)
+            }
+            //https://sebi.io/demo-image-api/pictures.json
+            AnimatedVisibility(visible = showImage){
+                KamelImage(
+                    asyncPainterResource("https://sebi.io/demo-image-api/pigeon/" +
+                            "vladislav-nikonov-yVYaUSwkTOs-unsplash.jpg"),
+                    contentDescription = null
+                )
+            }
         }
     }
 }
