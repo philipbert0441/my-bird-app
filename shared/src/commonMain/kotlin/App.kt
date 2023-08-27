bd37e0e626f36b9d32092c7f4d58c2155960c2da
philipbert0441 <philipb0441@gmail.com>
2023-08-26 22:19:19 -0400

    Added Theme and style to app

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -2,23 +2,24 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.AbsoluteCutCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import dev.icerock.moko.mvvm.compose.getViewModel
@@ -28,8 +29,24 @@ import io.kamel.image.asyncPainterResource
 import model.BirdImage
 
 @Composable
+fun BirdAppTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colors = MaterialTheme.colors.copy(primary = Color.Black),
+        shapes = MaterialTheme.shapes.copy(
+            small = AbsoluteCutCornerShape(0.dp),
+            medium = AbsoluteCutCornerShape(0.dp),
+            large = AbsoluteCutCornerShape(0.dp)
+        )
+    ) {
+        content()
+    }
+}
+
+@Composable
 internal fun App() {
-    MaterialTheme {
+    BirdAppTheme {
         /*
         https://sebi.io/demo-image-api/pictures.json
         https://github.com/Kotlin/kotlinx.serialization
@@ -60,6 +77,14 @@ fun BirdPage(viewModel: BirdsViewModel) {
         ) {
             for (category in uiState.categories) {
                 Button(
+                    modifier = Modifier
+                        .aspectRatio(1.0f)
+                        .fillMaxSize()
+                        .weight(1.0f),
+                    elevation = ButtonDefaults.elevation(
+                        defaultElevation = 0.dp,
+                        focusedElevation = 0.dp
+                    ),
                     onClick = {
                         viewModel.selectedCategory(category)
                     }
