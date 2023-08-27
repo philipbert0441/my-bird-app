0ece8f32b0b0883616abaf35f1250ff234c41f68
philipbert0441 <philipb0441@gmail.com>
2023-08-26 21:37:35 -0400

    Added Composables and lazygrid

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,51 +1,76 @@
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import dev.icerock.moko.mvvm.compose.getViewModel
+import dev.icerock.moko.mvvm.compose.viewModelFactory
 import io.kamel.image.KamelImage
 import io.kamel.image.asyncPainterResource
+import model.BirdImage
 
 @Composable
 internal fun App() {
     MaterialTheme {
-        var text by remember { mutableStateOf("Hello, Philip!") }
-        var showImage by remember { mutableStateOf(false) }
-        Column(
-            Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ){
-            Button(onClick = {
-                showImage = !showImage
-                text = if (showImage) {
-                    "Hello, ${getPlatformName()}"
-                } else {
-                    "Hello, Philip!"
-                }
-            }) {
-                Text(text)
-            }
-            //https://sebi.io/demo-image-api/pictures.json
-            //https://github.com/Kotlin/kotlinx.serialization
-            //https://ktor.io/docs/serialization-client.html#register_protobuf
-            //https://github.com/icerockdev/moko-mvvm
-            AnimatedVisibility(visible = showImage){
-                KamelImage(
-                    asyncPainterResource("https://sebi.io/demo-image-api/pigeon/" +
-                            "vladislav-nikonov-yVYaUSwkTOs-unsplash.jpg"),
-                    contentDescription = null
-                )
-            }
-        }
+        /*
+        https://sebi.io/demo-image-api/pictures.json
+        https://github.com/Kotlin/kotlinx.serialization
+        https://ktor.io/docs/serialization-client.html#register_protobuf
+        https://github.com/icerockdev/moko-mvvm
+        https://developer.android.com/jetpack/compose/animation/
+            composables-modifiers#animatedvisibility
+        */
+        val birdsViewModel: BirdsViewModel = getViewModel(key = Unit,
+            factory = viewModelFactory { BirdsViewModel() })
+        BirdPage(birdsViewModel)
     }
+}
+
+@Composable
+fun BirdPage(viewModel: BirdsViewModel) {
+    val uiState by viewModel.uiState.collectAsState()
+    AnimatedVisibility(uiState.images.isNotEmpty()) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            horizontalArrangement = Arrangement.spacedBy(5.dp),
+            verticalArrangement = Arrangement.spacedBy(5.dp),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 5.dp),
+            content = {
+                items(uiState.images) {
+                    BirdImageCell(it)
+                }
+            }
+        )
+    }
+}
+
+@Composable
+fun BirdImageCell(image: BirdImage) {
+    KamelImage(
+        asyncPainterResource("https://sebi.io/demo-image-api/${image.path}"),
+        contentDescription = "${image.category} by ${image.author}",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier.fillMaxWidth(1.0f )
+    )
 }
 
 expect fun getPlatformName(): String
